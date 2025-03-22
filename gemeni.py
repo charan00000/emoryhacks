6577de6b26367997cd8f105be92f6f28dd517feb
charan00000 <charan.koyaguri@gmail.com>
2025-03-22 17:02:38 -0400

    q

--- gemeni.py
+++ gemeni.py
@@ -24,19 +24,11 @@ def conversation():
 
 
 def generate(input):
-    client = genai.Client(
-        api_key=os.getenv("GEMINI_KEY"),
+    client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash",
+        contents = input,
     )
-
-    model = "gemini-2.0-flash"
-    contents = [
-        types.Content(
-            role="user",
-            parts=[
-                types.Part.from_text(text=input),
-            ],
-        ),
-    ]
     generate_content_config = types.GenerateContentConfig(
         temperature=1,
         top_p=0.95,
@@ -45,12 +37,8 @@ def generate(input):
         response_mime_type="text/plain",
     )
 
-    for chunk in client.models.generate_content_stream(
-        model=model,
-        contents=contents,
-        config=generate_content_config,
-    ):
-        print(chunk.text, end="")
+    print(response.text)
 
 if __name__ == "__main__":
+    conversation()
     generate("I have a headache")
