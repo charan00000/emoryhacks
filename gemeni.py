200bfee5869c8bd129b257e04dd12a33253bcc73
charan00000 <charan.koyaguri@gmail.com>
2025-03-22 19:34:53 -0400

    update convo context management

--- gemeni.py
+++ gemeni.py
@@ -25,25 +25,17 @@ def conversation():
         print(message.parts[0].text)
 
 #dont use
-def generate(input):
-    """client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
-    print("client")"""
+def generate(input, history):
     generativeai.configure(api_key=os.getenv("GEMINI_KEY"))
     model_name = "tunedModels/filteredmedicaldata-hpwt1yv1lgxb"
     model = generativeai.GenerativeModel(model_name=model_name)
-    response = model.generate_content(input)
+    query = "past question-answers in conversation: "
+    for (question, answer) in history:
+        query += f"(question: {question}, answer: {answer}), "
+    query += f"current question: {input}. If you believe that you have enough information to pass along to the doctor, you may tell the user that."
+    response = model.generate_content(query)
     return response.text
-    """chat = client.chats.create(model = "gemini-2.0-flash")
-    response = chat.send_message(input)
-    generate_content_config = types.GenerateContentConfig(
-        temperature=1,
-        top_p=0.95,
-        top_k=40,
-        max_output_tokens=8192,
-        response_mime_type="text/plain",
-    )
-    return response.text"""
 
 
-
-print(generate("I have a headache"))
+if __name__ == 'main':
+    print(generate("I have a headache"))
