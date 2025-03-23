453b61aae0342f0987df761192b6fb8d9d634964
charan00000 <charan.koyaguri@gmail.com>
2025-03-23 09:45:15 -0400

    sign up and login

--- pages/login.py
+++ pages/login.py
@@ -24,6 +24,15 @@ def initialize_session_state():
 		st.session_state.current_user = Person("", "", "", "", "", "")
 
 initialize_session_state()
+def check_login(email, password):
+	conn = sqlite3.connect("emory_hack.db")
+	c = conn.cursor()
+	c.execute("SELECT * FROM users WHERE email = ? AND password = ?", (email, password))
+	data = c.fetchall()
+	conn.close()
+	if len(data) == 0:
+		return False
+	return True
 
 st.markdown(
     """
@@ -458,25 +467,36 @@ with login:
 	with st.form("login-form", clear_on_submit=True, border=True) as login_form:
 		email = st.text_input("*Email*", key="email-text")
 		password = st.text_input("*Password*", key="password-text")
-		submitted = st.form_submit_button("**Login**",type="secondary")
-		if submitted:
-			#login logic here
+		submitted = st.form_submit_button("**Log in**",type="secondary")
+		if submitted and check_login(email, password):
 			st.write(email, password,)
 			st.success("Login successful!")
 			switch_page("Home")
+		elif submitted:
+			st.write('Login failed')
 with signup:
 	with st.form("signup-form", clear_on_submit=True, border=True) as login_form:
 		email = st.text_input("*Email*", key="email-text2", placeholder="Email")
 		password = st.text_input("*Password*", key="password-text2", placeholder="Password")
 		verify_password = st.text_input("*Verify Password*", key="verify-password-text", placeholder="Type your password again")
-		submitted = st.form_submit_button("**Login**",type="secondary")
+		submitted = st.form_submit_button("**Sign up**",type="secondary")
 		if submitted:
-			#signup logic here
-			st.session_state.current_user.email = email
-			st.session_state.current_user.password = password
-			st.write(email, password,)
-			st.success("Signup successful!")
-			switch_page("profile information")
+			conn = sqlite3.connect("emory_hack.db")
+			c = conn.cursor()
+			c.execute("SELECT * FROM users WHERE email = ?", (email,))
+			existing_user = c.fetchone()
+			if existing_user:
+				st.error("User already exists")
+			else:
+				#signup logic here
+				st.session_state.current_user.email = email
+				st.session_state.current_user.password = password
+				if password != verify_password:
+					st.error("Passwords do not match")
+				else:
+					st.write(email, password,)
+					st.success("Signup successful!")
+					switch_page("profile information")
 bar, text, bar = st.columns(3)
 st.markdown("""
 	<h3>- OR -</h3>
